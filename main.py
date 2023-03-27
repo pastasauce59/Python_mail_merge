b15a10c2278f8abc385b7ac20b07220ef9dbd12a
Michael Primavera <pastasauce59@gmail.com>
2023-03-26 23:35:35 -0400

    opened invited_names.txt file and saved contents as a list, opened starting_letter.txt file and saved contents.

--- main.py
+++ main.py
@@ -6,3 +6,12 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    print(letter_contents)
